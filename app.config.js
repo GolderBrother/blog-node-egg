c373d2428d2e3fb81d10d6f85385c303c78e2fb7
GolderBrother <1204788939@qq.com>
2019-09-02 21:56:36 +0800

    update app config

--- app.config.js
+++ app.config.js
@@ -15,18 +15,17 @@ exports.APP = {
 	LIMIT: 10,
 	PORT: 8000,
 	ROOT_PATH: __dirname,
-	NAME: 'biaochenxuying',
-	URL: 'http://biaochenxuying.cn/main.html',
-	FRONT_END_PATH: path.join(__dirname, '..', 'biaochenxuying'),
+	NAME: 'GolderBrother',
+	URL: 'http://116.62.6.228',
+	FRONT_END_PATH: path.join(__dirname, '..', 'GolderBrother'),
 };
 
 exports.CROSS_DOMAIN = {
 	allowedOrigins: [
-		'http://biaochenxuying.cn/main.html',
-		'http://biaochenxuying.cn',
-		'https://github.com/biaochenxuying',
+		'http://116.62.6.228',
+		'https://github.com/GolderBrother',
 	],
-	allowedReferer: 'biaochenxuying',
+	allowedReferer: 'GolderBrother',
 };
 
 exports.MONGODB = {
@@ -38,39 +37,39 @@ exports.AUTH = {
 	data: argv.auth_data || {
 		user: 'root'
 	},
-	jwtTokenSecret: argv.auth_key || 'blog-node',
+	jwtTokenSecret: argv.auth_key || 'blog-node-egg',
 	defaultPassword: argv.auth_default_password || 'root',
 };
 
 exports.EMAIL = {
-	account: argv.email_account || 'your email address like : i@biaochenxuying',
+	account: argv.email_account || 'your email address like : 1204788939@qq.com',
 	password: argv.email_password || 'your email password',
-	from: 'https://github.com/biaochenxuying',
-	admin: 'biaochenxuying',
+	from: 'https://github.com/GolderBrother',
+	admin: 'GolderBrother',
 };
 
 exports.AKISMET = {
 	key: argv.akismet_key || 'your akismet Key',
-	blog: argv.akismet_blog || 'your akismet blog site, like: http://biaochenxuying.cn/main.html',
+	blog: argv.akismet_blog || 'your akismet blog site, like: http://116.62.6.228',
 };
 
 exports.GITHUB = {
-	username: 'biaochenxuying',
+	username: 'GolderBrother',
 	oauth_uri: 'https://github.com/login/oauth/authorize',
 	access_token_url: 'https://github.com/login/oauth/access_token',
 	// 获取 github 用户信息 url // eg: https://api.github.com/user?access_token=****&scope=&token_type=bearer
 	user_url: 'https://api.github.com/user',
 
 	// 请把生产环境的 redirect_url，client_id 和 client_secret 中的 "****", 换成自己创建的 OAuth App 的具体参数即可。
-	// // 生产环境
-	// redirect_url: 'http://biaochenxuying.cn/login',
-	// client_id: '*****',
-	// client_secret: '*****',
+	// 生产环境
+	redirect_url: 'http://116.62.6.228',
+	client_id: 'cfb7f07a30c09efe8f6d',
+	client_secret: '4f51c9aba079dd97fdeaa18e9f9802d466f2a3e0',
 
 	// 开发环境 （参数可以直接用，公供测试）
-	redirect_url: "http://localhost:3001/login",
-	client_id: "502176cec65773057a9e",
-	client_secret: "65d444de381a026301a2c7cffb6952b9a86ac235",
+	// redirect_url: "http://localhost:3001/login",
+	// client_id: "502176cec65773057a9e",
+	// client_secret: "65d444de381a026301a2c7cffb6952b9a86ac235",
 
 };
 
@@ -79,7 +78,7 @@ exports.ALIYUN = {
 };
 
 exports.BAIDU = {
-	site: argv.baidu_site || 'your baidu site domain like : biaochenxuying',
+	site: argv.baidu_site || 'your baidu site domain like : GolderBrother',
 	token: argv.baidu_token || 'your baidu seo push token',
 };
 
@@ -96,6 +95,6 @@ exports.INFO = {
 	// version: package.version,
 	// author: package.author,
 	// site: exports.APP.URL,
-	github: 'https://github.com/biaochenxuying',
-	powered: ['react', 'Nodejs', 'MongoDB', 'Express', 'Nginx'],
+	github: 'https://github.com/GolderBrother',
+	powered: ['react', 'Nodejs', 'MongoDB', 'Eggjs', 'Nginx'],
 };
