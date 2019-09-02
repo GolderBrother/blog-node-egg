ef74d44b4f3fb3fabdc2c59b029ebb70b4ddd68e
GolderBrother <1204788939@qq.com>
2019-09-02 21:24:04 +0800

    test

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,7 +1,7 @@
 const Controller = require('egg').Controller;
 class HomeController extends Controller {
     async index() {
-        this.ctx.body = 'Hello eggjs!666123'
+        this.ctx.body = 'Hello eggjs!666'
     }
 }
 
